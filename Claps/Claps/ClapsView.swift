aa8633b349b34d760730735e1239a20aa794fe4e
Hemang Shah <hemangshah.in@gmail.com>
2017-08-20 12:51:25 +0530

    First Commit 👋

---
+++ Claps/Claps/ClapsView.swift
@@ -0,0 +1,360 @@
+//
+//  ClapsView.swift
+//  Claps
+//
+//  Created by Hemang Shah on 8/17/17.
+//  Copyright © 2017 Hemang Shah. All rights reserved.
+//
+
+import UIKit
+
+internal extension UIView {
+    func asCircle() {
+        self.layer.cornerRadius = self.frame.size.width/2.0
+        self.layer.masksToBounds = true
+    }
+}
+
+internal extension UIColor {
+    class func RGB(r: CGFloat, g: CGFloat, b: CGFloat) -> UIColor {
+        return UIColor.init(red: r/255, green: g/255, blue: b/255, alpha: 1.0)
+    }
+}
+
+internal extension Int {
+    var abbreviated: String {
+        let abbrev = "KMBTPE"
+        return abbrev.characters.enumerated().reversed().reduce(nil as String?) { accum, tuple in
+            let factor = Double(self) / pow(10, Double(tuple.0 + 1) * 3)
+            let format = (factor.truncatingRemainder(dividingBy: 1)  == 0 ? "%.0f%@" : "%.1f%@")
+            return accum ?? (factor > 1 ? String(format: format, factor, String(tuple.1)) : nil)
+            } ?? String(self)
+    }
+}
+
+public enum ClapsViewStates {
+    case begin
+    case end
+    case clapping
+    case finalized
+}
+
+public protocol ClapsViewDelegate {
+    func clapsViewStateChanged(clapsView: ClapsView, state: ClapsViewStates, totalClaps: Int, currentClaps: Int)
+}
+
+public class ClapsView: UIView {
+    
+    public var delegate: ClapsViewDelegate?
+    
+    ///Inactive State Border Color. Default: .lightGray
+    public var inActiveStateBorderColor: UIColor = .lightGray {
+        didSet {
+            inActiveState()
+        }
+    }
+    
+    ///Active State Border Color.
+    public var activeStateBorderColor: UIColor = .RGB(r: 255.0, g: 207.0, b: 74.0) {
+        didSet {
+            activeState()
+        }
+    }
+    
+    ///Inside Background Color. Default: .clear
+    public var insideBakcgroundColor: UIColor = .clear {
+        didSet {
+            emojiLabel?.backgroundColor = insideBakcgroundColor
+        }
+    }
+    
+    ///Claps Background Color.
+    public var clapsLabelBackgroundColor: UIColor = .RGB(r: 255.0, g: 207.0, b: 74.0) {
+        didSet {
+            clapsLabel?.backgroundColor = clapsLabelBackgroundColor
+        }
+    }
+    
+    ///Claps Text Color. Default: .white
+    public var clapsLabelTextColor: UIColor = .white {
+        didSet {
+            clapsLabel?.textColor = clapsLabelTextColor
+        }
+    }
+    
+    ///Claps Font. Default: SystemFont = 12.0
+    public var clapsLabelFont: UIFont = UIFont.systemFont(ofSize: 12.0) {
+        didSet {
+            clapsLabel?.font = clapsLabelFont
+        }
+    }
+    
+    ///Clap Emoji. Default: 👋
+    public var emoji: String = "👋" {
+        didSet {
+            if emoji.isEmpty {
+                fatalError("\(String.init(describing: ClapsView.self)) requires an Emoji.")
+            } else {
+                emojiLabel?.text = emoji
+            }
+        }
+    }
+    
+    ///Increase/Decrease the size of Emoji. Default: 40.0 | Minimum: 20.0
+    public var emojiLabelFontSize: CGFloat = 40.0 {
+        didSet {
+            if emojiLabelFontSize >= 20.0 {
+                emojiLabel?.font = UIFont.init(name: "AppleColorEmoji", size: emojiLabelFontSize)
+            } else {
+                fatalError("\(String.init(describing: ClapsView.self)) requires 20.0 size for Emoji. This is the minimum size.")
+            }
+        }
+    }
+    
+    ///Show 1000 claps as 1k. Default: false
+    public var showClapsAbbreviated: Bool = false
+    
+    ///Total Claps Count by All the Users.
+    public var totalClaps: Int = 0
+    ///Current Claps Count By a User.
+    public var currentClaps: Int = 0
+    ///The maximum number of claps allowed.
+    public var maxClaps: Int = 50
+    ///i.e. currentClaps + iteratorClaps = new claps.
+    public var iteratorClaps: Int = 1
+    
+    //To show the exact claps each time.
+    fileprivate var lastClapsCount: Int = 0
+    
+    //Private Values
+    fileprivate var clapsLabel: UILabel? = nil
+    fileprivate var emojiLabel: UILabel? = nil
+    
+    fileprivate var animationTimer: Timer? = nil
+    
+    fileprivate let animationDuration: TimeInterval = 0.4
+    fileprivate let timerDuration: TimeInterval = 0.5
+    
+    //MARK: Init
+    public override func awakeFromNib() {
+        setup()
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    //MARK: Setup
+    fileprivate func setup() {
+        if self.frameValidator() {
+            self.backgroundColor = UIColor.white
+            self.asCircle()
+            self.addEmojiLabel()
+            self.inActiveState()
+            self.addLongPressGesture()
+            self.addClapsLabel()
+        }
+    }
+    
+    //MARK: Gestures
+    fileprivate func addLongPressGesture() {
+        let longPressGesture = UILongPressGestureRecognizer.init(target: self, action: #selector(userTapped))
+        longPressGesture.minimumPressDuration = 0.0
+        longPressGesture.numberOfTouchesRequired = 1
+        longPressGesture.allowableMovement = 0
+        self.addGestureRecognizer(longPressGesture)
+    }
+    
+    //MARK: Gesture Action
+    @objc fileprivate func userTapped(longPressGesture: UILongPressGestureRecognizer) {
+        
+        if longPressGesture.state == .began {
+            
+            self.updateForBeginState()
+            self.countManagement()
+            self.activeState()
+            self.runTimer()
+            
+        } else if longPressGesture.state == .ended {
+            
+            self.updateForEndState()
+            self.stopTimer()
+            self.finalizeClaps()
+            self.inActiveState()
+        }
+    }
+    
+    //MARK: Update for Begin/End States
+    fileprivate func updateForBeginState() {
+        if (self.delegate != nil) {
+            self.delegate?.clapsViewStateChanged(clapsView: self, state: .begin, totalClaps: self.totalClaps, currentClaps: self.currentClaps)
+        }
+    }
+    
+    fileprivate func updateForEndState() {
+        if (self.delegate != nil) {
+            self.delegate?.clapsViewStateChanged(clapsView: self, state: .end, totalClaps: self.totalClaps, currentClaps: self.currentClaps)
+        }
+    }
+    
+    //MARK: Timer Management (Start/Stop)
+    fileprivate func runTimer() {
+        
+        self.stopTimer()
+        
+        if #available(iOS 10.0, *) {
+            self.animationTimer = Timer.scheduledTimer(withTimeInterval: self.timerDuration, repeats: true) { (timer) in
+                self.animateClapsLabel()
+            }
+        } else {
+            // Fallback on earlier versions
+            self.animationTimer = Timer.scheduledTimer(timeInterval: self.timerDuration, target: self, selector: #selector(animateClapsLabel), userInfo: nil, repeats: true)
+        }
+    }
+    
+    fileprivate func stopTimer() {
+        if let timer = animationTimer {
+            timer.invalidate()
+        }
+    }
+    
+    //MARK: Finalize Claps
+    internal func finalizeClaps() {
+        //Animate the final value to the user.
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + timerDuration, execute: {
+            let differenceInClapsCount = self.currentClaps - self.lastClapsCount
+            self.totalClaps = self.totalClaps + differenceInClapsCount
+            
+            if self.showClapsAbbreviated {
+                self.clapsLabel?.text = self.totalClaps.abbreviated
+            } else {
+                self.clapsLabel?.text = "\(self.totalClaps)"
+            }
+            
+            if (self.delegate != nil) {
+                self.delegate?.clapsViewStateChanged(clapsView: self, state: .finalized, totalClaps: self.totalClaps, currentClaps: self.currentClaps)
+            }
+            
+            UIView.animate(withDuration: self.animationDuration, delay: 0.0, options: .curveEaseOut, animations: {
+                self.clapsLabel?.alpha = 1.0
+                self.clapsLabel?.transform = CGAffineTransform(scaleX: 3.0,y: 3.0)
+            }) { (isCompleted) in
+                self.clapsLabel?.alpha = 0.0
+                self.clapsLabel?.transform = CGAffineTransform(scaleX: 1.0,y: 1.0)
+            }
+        })
+    }
+    
+    ///This is to remove a user claps. So we will requires to update the ClapsView with new claps.
+    internal func remove(withTotalClaps totalClaps: Int) {
+        self.totalClaps = totalClaps
+        self.currentClaps = 0
+        self.lastClapsCount = 0
+        self.countManagement()
+        self.finalizeClaps()
+    }
+    
+    //MARK: Iterator
+    @objc fileprivate func animateClapsLabel() {
+        
+        self.iterateClaps()
+        
+        UIView.animate(withDuration: animationDuration, delay: 0.0, options: .curveEaseOut, animations: {
+            self.clapsLabel?.alpha = 1.0
+            self.clapsLabel?.transform = CGAffineTransform(scaleX: 3.0,y: 3.0)
+            self.emojiLabel?.transform = CGAffineTransform(scaleX: 1.5,y: 1.5)
+        }) { (isCompleted) in
+            self.clapsLabel?.alpha = 0.0
+            self.clapsLabel?.transform = CGAffineTransform(scaleX: 1.0,y: 1.0)
+            self.emojiLabel?.transform = CGAffineTransform(scaleX: 1.0,y: 1.0)
+        }
+    }
+    
+    fileprivate func countManagement() {
+        if self.lastClapsCount >= 0 {
+            self.lastClapsCount = self.currentClaps
+        }
+    }
+    
+    fileprivate func iterateClaps() {
+        if !self.hasMaxClaps() {
+            self.currentClaps = self.currentClaps + self.iteratorClaps
+
+            if (self.delegate != nil) {
+                self.delegate?.clapsViewStateChanged(clapsView: self, state: .clapping, totalClaps: self.totalClaps, currentClaps: self.currentClaps)
+            }
+            
+        }
+        
+        if self.showClapsAbbreviated {
+            self.clapsLabel?.text = self.currentClaps.abbreviated
+        } else {
+            self.clapsLabel?.text = "\(self.totalClaps)"
+            self.clapsLabel?.text = String(self.currentClaps)
+        }
+                
+    }
+    
+    internal func hasMaxClaps() -> Bool {
+        return (self.currentClaps == self.maxClaps)
+    }
+    
+    //MARK: UI Helpers
+    fileprivate func initialFrame() -> CGRect {
+        return CGRect.init(origin: .zero, size: CGSize.init(width: self.frame.size.width, height: self.frame.size.height))
+    }
+    
+    fileprivate func addEmojiLabel() {
+        let label = UILabel.init(frame: initialFrame())
+        label.asCircle()
+        label.backgroundColor = insideBakcgroundColor
+        label.isUserInteractionEnabled = false
+        label.text = emoji
+        label.font = UIFont.init(name: "AppleColorEmoji", size: emojiLabelFontSize)
+        label.textAlignment = .center
+        label.clipsToBounds = false
+        self.addSubview(label)
+        label.center = CGPoint.init(x: self.frame.size.width/2.0, y: self.frame.size.height/2.0)
+        emojiLabel = label
+    }
+    
+    fileprivate func addClapsLabel() {
+        let label = UILabel.init(frame: initialFrame())
+        label.asCircle()
+        label.backgroundColor = clapsLabelBackgroundColor
+        label.isUserInteractionEnabled = false
+        label.text = String(totalClaps)
+        label.font = clapsLabelFont
+        label.adjustsFontSizeToFitWidth = true
+        label.textColor = clapsLabelTextColor
+        label.textAlignment = .center
+        label.alpha = 0.0
+        self.addSubview(label)
+        label.center = CGPoint.init(x: self.frame.size.width/2.0, y: self.frame.size.height/2.0)
+        clapsLabel = label
+    }
+    
+    fileprivate func activeState() {
+        self.layer.borderColor = activeStateBorderColor.cgColor
+        self.layer.borderWidth = 2.0
+    }
+    
+    fileprivate func inActiveState() {
+        self.layer.borderColor = inActiveStateBorderColor.cgColor
+        self.layer.borderWidth = 2.0
+    }
+    
+    //MARK: Validator
+    fileprivate func frameValidator() -> Bool {
+        let size = self.frame.size
+        if size.width != size.height {
+            fatalError("\(String.init(describing: ClapsView.self)) width and height should be equal.")
+        }
+        return true
+    }
+}
---
+++ Claps/Claps/ViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ViewController.swift
+//  Claps
+//
+//  Created by Hemang Shah on 8/17/17.
+//  Copyright © 2017 Hemang Shah. All rights reserved.
+//
+
+import UIKit
+
+let kClapsForImageView = "kClapsForImageView"
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.addImages()
+    }
+
+    fileprivate func addImages() {
+        let scrollView = UIScrollView.init(frame: CGRect.init(origin: CGPoint.zero, size: UIScreen.main.bounds.size))
+        scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight, .flexibleLeftMargin, .flexibleRightMargin, .flexibleTopMargin, .flexibleBottomMargin]
+        scrollView.autoresizesSubviews = true
+        scrollView.isPagingEnabled = true
+        scrollView.showsHorizontalScrollIndicator = true
+        self.view.addSubview(scrollView)
+        
+        let images = [UIImage.init(named: "1.jpg"), UIImage.init(named: "2.jpg"), UIImage.init(named: "3.jpg"), UIImage.init(named: "4.jpg"), UIImage.init(named: "5.jpg")]
+        
+        var originX: CGFloat = 0.0
+        let originY: CGFloat = 0.0
+        
+        let imageViewWidth = scrollView.frame.size.width
+        let imageViewHeight = scrollView.frame.size.height
+        
+        let margin: CGFloat = 20.0
+        let size: CGFloat = 100.0
+        var clapsViewTag = 1
+        
+        for image in images {
+            
+            //Adding UIImageView
+            let imageView = UIImageView.init(frame: CGRect.init(origin: CGPoint.init(x: originX, y: originY), size: CGSize.init(width: imageViewWidth, height: imageViewHeight)))
+            imageView.image = image
+            imageView.contentMode = .scaleAspectFill
+            imageView.clipsToBounds = true
+            scrollView.addSubview(imageView)
+            
+            //Adding ClapsView
+            let claps = ClapsView.init(frame: CGRect.init(origin: CGPoint.init(x: originX + margin, y: imageViewHeight - (size + margin)), size: CGSize.init(width: size, height: size)))
+            claps.delegate = self
+            //Emoji to be used with ClapsView.
+            claps.emoji = "👋"
+            //The number of claps by all of the user.
+            claps.totalClaps = 30
+            //The maximum number of claps a user can do.
+            claps.maxClaps = 50
+            //If set true, 1000 claps will be display as 1k
+            claps.showClapsAbbreviated = true
+            claps.autoresizingMask = [.flexibleLeftMargin, .flexibleRightMargin, .flexibleTopMargin, .flexibleBottomMargin]
+            //Tag for the identification.
+            claps.tag = clapsViewTag
+            scrollView.addSubview(claps)
+            //If there are any claps it will be visible to user.
+            //The same behavior can be achieved by tapping on a ClapsView.
+            claps.finalizeClaps()
+
+            //Loading Previous Claps for the ClapsView.
+            loadPreviousClaps(forClapsView: claps)
+
+            clapsViewTag = clapsViewTag + 1
+            originX = originX + scrollView.frame.size.width
+        }
+        scrollView.contentSize = CGSize.init(width: CGFloat(images.count) * imageViewWidth, height: imageViewHeight)
+    }
+    
+    //MARK: Handle Claps.
+    //In live apps, generally this should be handle using the server.
+    fileprivate func loadPreviousClaps(forClapsView clapsView: ClapsView) {
+        //Here instead of user defaults, you can load the claps by a user which is stored on your server.
+        let defaults = UserDefaults.standard
+        if let claps = defaults.value(forKey: getKeyForClapsView(forClapsView: clapsView)) {
+            clapsView.currentClaps = claps as! Int
+        }
+    }
+    
+    fileprivate func saveClapsForClapsView(clapsView: ClapsView, withClaps claps: Int) {
+        //Here instead of user defaults to save a user claps, you can store it to the server.
+        let defaults = UserDefaults.standard
+        defaults.set(claps, forKey: getKeyForClapsView(forClapsView: clapsView))
+        defaults.synchronize()
+    }
+    
+    fileprivate func getKeyForClapsView(forClapsView clapsView: ClapsView) -> String {
+        return "\(kClapsForImageView)\(clapsView.tag)"
+    }
+}
+
+extension ViewController: ClapsViewDelegate {
+    func clapsViewStateChanged(clapsView: ClapsView, state: ClapsViewStates, totalClaps: Int, currentClaps: Int) {
+        if state == .finalized {
+            print("ClapsViewTag: \(clapsView.tag) | Total Claps:\(totalClaps) | Current Claps:\(currentClaps)")
+            saveClapsForClapsView(clapsView: clapsView, withClaps: currentClaps)
+        }
+    }
+}

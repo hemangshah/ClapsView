b310c312a2c8f508854553f036b8b420a4d8f1a8
Hemang Shah <hemangshah.in@gmail.com>
2017-10-10 16:28:24 +0530

    Simple example

--- Claps/Claps/ViewController.swift
+++ Claps/Claps/ViewController.swift
@@ -13,70 +13,15 @@ let kClapsForImageView = "kClapsForImageView"
 class ViewController: UIViewController {
     
     @IBOutlet var clapsView: ClapsView!
-
+    @IBOutlet var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        //Comment below line to see ClapsView created in UIStoryboard.
-        self.addImages()
-    }
-
-    fileprivate func addImages() {
-        let scrollView = UIScrollView.init(frame: CGRect.init(origin: CGPoint.zero, size: UIScreen.main.bounds.size))
-        scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight, .flexibleLeftMargin, .flexibleRightMargin, .flexibleTopMargin, .flexibleBottomMargin]
-        scrollView.autoresizesSubviews = true
-        scrollView.isPagingEnabled = true
-        scrollView.showsHorizontalScrollIndicator = true
-        self.view.addSubview(scrollView)
-        
-        let images = [UIImage.init(named: "1.jpg"), UIImage.init(named: "2.jpg"), UIImage.init(named: "3.jpg"), UIImage.init(named: "4.jpg"), UIImage.init(named: "5.jpg")]
-        
-        var originX: CGFloat = 0.0
-        let originY: CGFloat = 0.0
-        
-        let imageViewWidth = scrollView.frame.size.width
-        let imageViewHeight = scrollView.frame.size.height
-        
-        let margin: CGFloat = 20.0
-        let size: CGFloat = 100.0
-        var clapsViewTag = 1
-        
-        for image in images {
-            
-            //Adding UIImageView
-            let imageView = UIImageView.init(frame: CGRect.init(origin: CGPoint.init(x: originX, y: originY), size: CGSize.init(width: imageViewWidth, height: imageViewHeight)))
-            imageView.image = image
-            imageView.contentMode = .scaleAspectFill
-            imageView.clipsToBounds = true
-            scrollView.addSubview(imageView)
-            
-            //Adding ClapsView
-            let claps = ClapsView.init(frame: CGRect.init(origin: CGPoint.init(x: originX + margin, y: imageViewHeight - (size + margin)), size: CGSize.init(width: size, height: size)))
-            claps.delegate = self
-            //Emoji to be used with ClapsView.
-            claps.emoji = "👏"
-            //The number of claps by all of the user.
-            claps.totalClaps = 30
-            //The maximum number of claps a user can do.
-            claps.maxClaps = 50
-            //If set true, 1000 claps will be display as 1k
-            claps.showClapsAbbreviated = true
-            claps.autoresizingMask = [.flexibleLeftMargin, .flexibleRightMargin, .flexibleTopMargin, .flexibleBottomMargin]
-            //Tag for the identification.
-            claps.tag = clapsViewTag
-            scrollView.addSubview(claps)
-            //If there are any claps it will be visible to user.
-            //The same behavior can be achieved by tapping on a ClapsView.
-            claps.finalizeClaps()
-
-            //Loading Previous Claps for the ClapsView.
-            loadPreviousClaps(forClapsView: claps)
-
-            clapsViewTag = clapsViewTag + 1
-            originX = originX + scrollView.frame.size.width
-        }
-        scrollView.contentSize = CGSize.init(width: CGFloat(images.count) * imageViewWidth, height: imageViewHeight)
+        self.webView.loadRequest(URLRequest.init(url: URL.init(string: "https://blog.medium.com/show-authors-more-%EF%B8%8F-with-s-c1652279ba01")!))
+        self.loadPreviousClaps(forClapsView: self.clapsView)
+        self.clapsView.finalizeClaps()
     }
     
     //MARK: Handle Claps.
@@ -105,7 +50,7 @@ extension ViewController: ClapsViewDelegate {
     func clapsViewStateChanged(clapsView: ClapsView, state: ClapsViewStates, totalClaps: Int, currentClaps: Int) {
         if state == .finalized {
             print("ClapsViewTag: \(clapsView.tag) | Total Claps:\(totalClaps) | Current Claps:\(currentClaps)")
-            saveClapsForClapsView(clapsView: clapsView, withClaps: currentClaps)
+            self.saveClapsForClapsView(clapsView: clapsView, withClaps: currentClaps)
         }
     }
 }

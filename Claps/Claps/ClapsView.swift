cc211c714c8e1a66f5bdfed9a66f8100f3102726
Hemang <hemangshah.in@gmail.com>
2017-12-21 18:34:46 +0530

    Updated for Swift 4.x

--- Claps/Claps/ClapsView.swift
+++ Claps/Claps/ClapsView.swift
@@ -24,7 +24,7 @@ fileprivate extension UIColor {
 fileprivate extension Int {
     var abbreviated: String {
         let abbrev = "KMBTPE"
-        return abbrev.characters.enumerated().reversed().reduce(nil as String?) { accum, tuple in
+        return abbrev.enumerated().reversed().reduce(nil as String?) { accum, tuple in
             let factor = Double(self) / pow(10, Double(tuple.0 + 1) * 3)
             let format = (factor.truncatingRemainder(dividingBy: 1)  == 0 ? "%.0f%@" : "%.1f%@")
             return accum ?? (factor > 1 ? String(format: format, factor, String(tuple.1)) : nil)
